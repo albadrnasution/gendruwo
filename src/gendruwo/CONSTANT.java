795f378a912e2fc526e2ab3be703e18381e9bc25
m.ln.albadr@gmail.com <m.ln.albadr@gmail.com@08e58d39-1f43-f9e6-dfe3-0ccae02669d4>
2011-11-30 02:13:43 +0000

    Angka-angka yang digunakan gan!

--- src/gendruwo/CONSTANT.java
+++ src/gendruwo/CONSTANT.java
@@ -12,26 +12,26 @@ public class CONSTANT {
     /*Pembatasan maksimum generasi yang mungkin*/
     public static final int MAX_GENERATION = 888;
     
-    /*Persentasi populasi yang dimutasi*/
-    public static final float MUTATION_RATE = 0.8f;
+    /*Fraksi populasi yang dimutasi terhadap jumlah populasi saat itu*/
+    public static final float MUTATION_RATE = 0.08f;    /*8%*/
     
     /*Peluang sebuah bit itu dimutasi/diflip atau tidak*/
-    public static final float MUTATION_PROB = 0.8f;
+    public static final float MUTATION_PROB = 0.08f;
     
     /*Penurunan populasi dari jumlah data set sampai jumlah terminal*/
-    public static final float DECAY_POP_RATE = 0.008f;
+    public static final float DECAY_POP_RATE = 0.008f;    /*0.8%*/
     
     /*Jumlah terminal populasi dibanding jumlah awal populasi*/
-    public static final float TERMINAL_POP_FROM_INITIAL = 0.08f;
+    public static final float TERMINAL_POP_FROM_INITIAL = 0.08f;    /*8%*/
     
     /*Peluang minimum dan maksimum sebuah individu/kromosom terambil untuk cross-over*/
-    public static final float COVER_RATE_MIN = 0.68f;
-    public static final float COVER_RATE_MAX = 0.86f;
+    public static final float COVER_RATE_MIN = 0.68f;       /*68%*/
+    public static final float COVER_RATE_MAX = 0.86f;       /*86%*/
     
     /*Presentasi jumlah PASANGAN dari jumlah populasi yang mungkin saat menaik-turunkan populasi*/
-    public static final float COVER_COUPLE_MIN = 0.48f;
-    public static final float COVER_COUPLE_MAX = 0.84f;
+    public static final float COVER_COUPLE_MIN = 0.48f;     /*48%*/
+    public static final float COVER_COUPLE_MAX = 0.84f;     /*84%*/
     
     /*Besar persentasu akurasi yang diinginkan*/
-    public static float DESIRED_ACCURATION = 0.8f;
+    public static float DESIRED_ACCURATION = 0.8f;          /*80%*/
 }
