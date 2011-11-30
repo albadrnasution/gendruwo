5f079314f025c4eb87bd447a1b4f58fddccd215f
sidiksoleman@gmail.com <sidiksoleman@gmail.com@08e58d39-1f43-f9e6-dfe3-0ccae02669d4>
2011-11-30 12:10:37 +0000

    no thing to lose 1. midahno atribut dari GA ke individu 2. nambahi komen titik. wis kui thok.

--- trunk/src/gendruwo/GA.java
+++ trunk/src/gendruwo/GA.java
@@ -20,31 +20,7 @@ public class GA {
     ArrayList<Individu> rules = new ArrayList<Individu>();
 
     public GA(){
-        //ngisi atribut
-        att.add(new Attribute("edibility",0,0,"ep"));
-        att.add(new Attribute("cap-shape",1,3,"bcxfks"));
-        att.add(new Attribute("cap-surface",4,6,"fgys"));
-        att.add(new Attribute("cap-color",7,10,"nbcgrpuewy"));
-        att.add(new Attribute("bruises",11,12,"tf"));
-        att.add(new Attribute("odor",13,16,"alcyfmnps"));
-        att.add(new Attribute("gill-attachment",17,19,"adfn"));
-        att.add(new Attribute("gill-spacing",20,21,"cwd"));
-        att.add(new Attribute("gill-size",22,23,"bn"));
-        att.add(new Attribute("gill-color",24,27,"knbhgropuewy"));
-        att.add(new Attribute("stalk-shape",28,30,"et"));
-        att.add(new Attribute("stalk-root",31,33,"bcuezr?"));
-        att.add(new Attribute("stalk-surface-above-ring",34,36,"fyks"));
-        att.add(new Attribute("stalk-surface-below-ring",37,39,"fyks"));
-        att.add(new Attribute("stalk-color-above-ring",40,43,"nbcgopewy"));
-        att.add(new Attribute("stalk-color-below-ring",44,47,"nbcgopewy"));
-        att.add(new Attribute("veil-type",48,49,"pu"));
-        att.add(new Attribute("veil-color",50,52,"nowy"));
-        att.add(new Attribute("ring-number",53,54,"not"));
-        att.add(new Attribute("ring-type",55,58,"ceflnpsz"));
-        att.add(new Attribute("spore-print-color",59,62,"knbhrouwy"));
-        att.add(new Attribute("population",63,65,"acnsvy"));
-        att.add(new Attribute("habitat",66,68,"glmpuwd"));
-        
+       
     }
 
     public void bacaTraining(){
@@ -91,6 +67,11 @@ public class GA {
         
     }
 
+    
+    public void secondStage(){
+        
+    }
+            
     public void doGA(){
         //melakukan serangkaian genetic algorithm hingga seleksi
         //definisi presentase --> breeding : mutasi = 15:1
--- trunk/src/gendruwo/Individu.java
+++ trunk/src/gendruwo/Individu.java
@@ -2,46 +2,76 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gendruwo;
 
 import java.util.BitSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
  * @author Hendra
  */
 public class Individu extends BitSet {
-    
-    
+    //list atribut yang dimiliki oleh individu
+    //ceritanya teh, individu punya atribut 
+
+    static final ArrayList<Attribute> attributes = new ArrayList<Attribute>(Arrays.asList(
+            new Attribute("edibility", 0, 0, "ep"),
+            new Attribute("cap-shape", 1, 3, "bcxfks"),
+            new Attribute("cap-surface", 4, 6, "fgys"),
+            new Attribute("cap-color", 7, 10, "nbcgrpuewy"),
+            new Attribute("bruises", 11, 12, "tf"),
+            new Attribute("odor", 13, 16, "alcyfmnps"),
+            new Attribute("gill-attachment", 17, 19, "adfn"),
+            new Attribute("gill-spacing", 20, 21, "cwd"),
+            new Attribute("gill-size", 22, 23, "bn"),
+            new Attribute("gill-color", 24, 27, "knbhgropuewy"),
+            new Attribute("stalk-shape", 28, 30, "et"),
+            new Attribute("stalk-root", 31, 33, "bcuezr?"),
+            new Attribute("stalk-surface-above-ring", 34, 36, "fyks"),
+            new Attribute("stalk-surface-below-ring", 37, 39, "fyks"),
+            new Attribute("stalk-color-above-ring", 40, 43, "nbcgopewy"),
+            new Attribute("stalk-color-below-ring", 44, 47, "nbcgopewy"),
+            new Attribute("veil-type", 48, 49, "pu"),
+            new Attribute("veil-color", 50, 52, "nowy"),
+            new Attribute("ring-number", 53, 54, "not"),
+            new Attribute("ring-type", 55, 58, "ceflnpsz"),
+            new Attribute("spore-print-color", 59, 62, "knbhrouwy"),
+            new Attribute("population", 63, 65, "acnsvy"),
+            new Attribute("habitat", 66, 68, "glmpuwd")));
+
     /**
      * Membandingkan dua individu kromosom.
-     * Sama jika ???????????
+     * Individu yang manggil adalah individu bakal rules aka yang masih abu-abu
+     * Sama jika 
      * @param individu
      * @param att
      * @return 
      */
-    public boolean compare(Individu individu, ArrayList<Attribute> att){
-        for(int i=0; i<att.size(); ++i){
-            int part1Int = att.get(i).getPartInt(this);
-            int part2Int = att.get(i).getPartInt(individu);
-            if(part2Int>att.get(i).pilihan.length())
-                return true; //tidak termasuk pilihan yang ada --> don't care
-            else return (part1Int == part2Int);
+    public boolean compare(Individu individuTraining) {
+        boolean different = false;
+        for (int i = 0; i < attributes.size() && !different; ++i) {
+            int part1Int = attributes.get(i).getPartInt(this);
+            int part2Int = attributes.get(i).getPartInt(individuTraining);
+            if (part2Int > attributes.get(i).pilihan.length()) {
+                different= true; //tidak termasuk pilihan yang ada --> don't care
+            } else {
+                different=(part1Int == part2Int);
+            }
         }
-        return false;
+        return different;
     }
-    
+
     /**
      * Mengeset bit dari pasangan ke bit dalam individu, dari bit index awal ke indeex akhir.
      * @param pasangan
      * @param awal
      * @param akhir 
      */
-    public void set(Individu pasangan, int awal, int akhir){
-        for(int i=awal; i<=akhir; ++i)
-            this.set(i,pasangan.get(i));
+    public void set(Individu pasangan, int awal, int akhir) {
+        for (int i = awal; i <= akhir; ++i) {
+            this.set(i, pasangan.get(i));
+        }
     }
-
 }
