fcb5404cf9c91a9defe814151c94a19d6ab297a8
m.ln.albadr@gmail.com <m.ln.albadr@gmail.com@08e58d39-1f43-f9e6-dfe3-0ccae02669d4>
2011-11-30 19:45:19 +0000

    Minor bug and CONSTANT enhancement

--- trunk/src/gendruwo/CONSTANT.java
+++ trunk/src/gendruwo/CONSTANT.java
@@ -19,7 +19,7 @@ public class CONSTANT {
     public static final float MUTATION_PROB = 0.08f;
     
     /*Penurunan populasi dari jumlah data set sampai jumlah terminal*/
-    public static final float DECAY_POP_RATE = 0.008f;    /*0.8%*/
+    public static final float DECAY_POP_RATE = 0.18f;    /*0.8%*/
     
     /*Jumlah terminal populasi dibanding jumlah awal populasi*/
     public static final float TERMINAL_POP_FROM_INITIAL = 0.08f;    /*8%*/
@@ -33,7 +33,7 @@ public class CONSTANT {
     public static final float COVER_COUPLE_MAX = 0.86f;     /*84%*/
     
     /*Besar persentasu akurasi yang diinginkan*/
-    public static float DESIRED_ACCURATION = 0.8f;          /*80%*/
+    public static float DESIRED_ACCURATION = 0.80f;          /*80%*/
     
     /*Besar persentasu akurasi yang diinginkan*/
     public static int CHROMOSOME_LEN = 68;          /*80%*/
